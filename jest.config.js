af10a4943f01fba5c977c1e1d838497e95453923
Refat C. <98761937+refat-chowdhury-sazim@users.noreply.github.com>
2025-11-12 22:11:33 +0600

    feat: upadte workflows for k8s (#69)

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,20 @@ module.exports = {
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/$1",
   },
+
+  maxWorkers: process.env.CI ? "50%" : "75%",
+  cache: true,
+  cacheDirectory: "<rootDir>/.jest-cache",
+  testTimeout: 10000,
+
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+      tsconfig: {
+        jsx: "react",
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    },
+  },
 };
